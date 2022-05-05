4bfa40537cb07c7a55123dc248b9b82daedf9aa1
Hannah Hazi <hannah@anvil.works>
2022-05-05 10:30:47 +0100

    Try to get around unreliable WiFi issue.

--- examples/anvil_uplink.py
+++ examples/anvil_uplink.py
@@ -17,12 +17,9 @@ if "CLIENT_UPLINK_KEY" in os.environ:
     # Works if you've saved the uplink key on your Raspberry Pi already
     CLIENT_UPLINK_KEY = os.environ["CLIENT_UPLINK_KEY"]
 else:
-    # Alternatively, fill in your Anvil app's Client Uplink Key here.
+    # Alternatively, fill in your Anvil app's Client Uplink Key here:
     CLIENT_UPLINK_KEY = "YOUR CLIENT UPLINK KEY HERE"
-    # Remember, your key is a secret,
-    # so make sure not to publish it when you publish this code!
-
-anvil.server.connect(CLIENT_UPLINK_KEY)
+    # Remember, your key is a secret, so make sure not to share it when you publish this code!
 
 # Read the BME280 and discard the initial nonsense readings
 sensor.update(interval=10.0)
@@ -71,32 +68,35 @@ try:
             'Pressure': sensor.pressure,
             }
 
-        anvil.server.call('store_latest_weather_hat_data', weather_data_dict)
+        try:
+            anvil.server.connect(CLIENT_UPLINK_KEY)
+            anvil.server.call('store_latest_weather_hat_data', weather_data_dict)
+            print(f"""
+                Uploading weather data...
 
-        print(f"""
-    Uploading weather data...  
-    
-    System temp: {sensor.device_temperature:0.2f} *C
-    Sensor temp offset: {sensor.temperature_offset}
-    Temperature: {sensor.temperature:0.2f} *C
-    Humidity:    {sensor.humidity:0.2f} %
-    Dew point:   {sensor.dewpoint:0.2f} *C
-    Light:       {sensor.lux:0.2f} Lux
-    Pressure:    {sensor.pressure:0.2f} hPa
-    Wind (avg):  {sensor.wind_speed:0.2f} m/sec
-    Rain:        {sensor.rain:0.2f} mm/sec
-    Wind (avg):  {sensor.wind_direction:0.2f} degrees ({wind_direction_cardinal})
-    
-    Ctrl+C to exit
-    
-    """)
+                System temp: {sensor.device_temperature:0.2f} *C
+                Sensor temp offset: {sensor.temperature_offset}
+                Temperature: {sensor.temperature:0.2f} *C
+                Humidity:    {sensor.humidity:0.2f} %
+                Dew point:   {sensor.dewpoint:0.2f} *C
+                Light:       {sensor.lux:0.2f} Lux
+                Pressure:    {sensor.pressure:0.2f} hPa
+                Wind (avg):  {sensor.wind_speed:0.2f} m/sec
+                Rain:        {sensor.rain:0.2f} mm/sec
+                Wind (avg):  {sensor.wind_direction:0.2f} degrees ({wind_direction_cardinal})
+
+                Ctrl+C to exit
+
+                """)
+            anvil.server.disconnect()
+        except Exception as e:
+            print("Upload failed due to: {}".format(e))
+            print("Will try again in 5 minutes.")
 
         sleep(300.0)  # Then store data again every 5 min
 
 except KeyboardInterrupt:
     print("Finished data upload. Restart script to continue.")
-except Exception as e:
-    print("Failed due to: {}".format(e))
 
 finally:
     done_image = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
